d2759a979642d503a0a2a4366c94ef4c697b26b9
Daniel Hernandez <dh.danny@hotmail.com>
2018-10-09 00:57:49 -0700

    Add bio

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,25 @@
 import React from 'react'
-import { Link } from 'gatsby'
 
-import Layout from '../components/layout'
+import './home.css'
 
 const IndexPage = () => (
-  <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
+    <div id="wrapper">
+      <div id="container">
+        <h1 id="title">Hello world.</h1>
+        <p>
+           <a href="https://www.linkedin.com/in/daniel-h-049445127/" target="_blank" rel="noopener noreferrer" id="name">Daniel Hernández</a> is a Venezuelan self-taught app developer with a passion for beautiful, simple web apps. Why complicate things more than they should be?
+        </p>
+        <p>
+          He started programming when he was 14, and has been at it ever since. Check out his <a href="https://github.com/DHDaniel" rel="noopener noreferrer" target="_blank">GitHub Profile</a> or his agency website, <a href="https://sunnysidecode.com/" rel="noopener noreferrer" target="_blank">Sunnyside Software</a>, to learn more about what makes him tick.
+        </p>
+        <p>
+          He arrived in San Francisco in August 2018 to study Computational Sciences and Business at the <a href="https://www.minerva.kgi.edu/" rel="noopener noreferrer" target="_blank">Minerva Schools at KGI</a>. And he is quite excited to attend his first ever hackathon, JAMStack, which is coincidentially hosted by the very <a href="https://www.freecodecamp.org/" rel="noopener noreferrer" target="_blank">same website</a> that he used to learn Node.js a couple years back!
+        </p>
+        <p>
+          And yes, he also thinks it's kind of weird to talk in the third person.
+        </p>
+      </div>
+    </div>
 )
 
 export default IndexPage
